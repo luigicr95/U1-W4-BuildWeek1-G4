c2f91d1110ee48845aeebdc5859de44bfa65d9ea
Luigi Indaco <luigindaco@gmail.com>
2022-09-21 13:16:28 +0200

    aggiunta burger menu

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0]
+const menuLista  = document.getElementById("menu-lista")[0]
+
+toggleButton.addEventListener("click", () => {
+navbarList.ClassList.toggle("active")
+})
