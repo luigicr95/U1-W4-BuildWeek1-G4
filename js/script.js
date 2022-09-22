0f73a2fd9639b769ba1982d07a66edca8b40d4b3
Aras Pourbijan <arasmidos@gmail.com>
2022-09-22 15:58:54 +0200

    until scroll

--- js/script.js
+++ js/script.js
@@ -4,3 +4,25 @@ const menuLista  = document.getElementsByClassName("menu-lista")[0];
 toggleButton.addEventListener("click", () => {
 menuLista.classList.toggle("active")
 })
+
+
+// const contact=document.querySelectorAll('#contanct');
+
+// window.addEventListener('scroll', checkEvent);
+
+// checkEvent();
+
+// function checkEvent(){
+//     const triggerbottom=console.log(window.innerHeight/5*4);
+
+// boxes.forEach(section => {
+//     const sectionTop= section.getBoundingClientRect().top;
+//     if(sectionTop <triggerbottom ){
+//       section.classList.add('show')   ;
+//     } else{
+//         section.classList..remove('show');
+//     }
+    
+// });
+     
+// }
