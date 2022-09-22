5bb4d43b89cf98c47df50e2f1e7e707488cb408e
Luigi Indaco <luigindaco@gmail.com>
2022-09-22 14:10:02 +0200

    aggiunta javascript

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@ const menuLista  = document.getElementsByClassName("menu-lista")[0];
 toggleButton.addEventListener("click", () => {
 menuLista.classList.toggle("active")
 })
+
+
+// ------------------------------------------------------
+
+window.addEventListener("scroll",() => {
+    let content = document.getElementById("carosello-bootstrap");
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = innerHeight /1;
+    if (contentPosition < screenPosition) {
+        content.getElementsByClassName("")[0];
+    } else {
+        content.classList.remove("");
+    }
+    });
+
+
+
+// -----------------------------------------------------
