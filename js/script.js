a0953c93afdc3f541eb13b90cd5281a68965e05a
luigicr95 <112467906+luigicr95@users.noreply.github.com>
2022-09-22 15:16:39 +0200

    trying observer

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const menuLista  = document.getElementsByClassName("menu-lista")[0];
+const menuLista = document.getElementsByClassName("menu-lista")[0];
 
 toggleButton.addEventListener("click", () => {
-menuLista.classList.toggle("active")
-})
+  menuLista.classList.toggle("active");
+});
+
+// Create the observer
+
+/*const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("dish-animation-running");
+      return;
+    }
+  });
+});
+
+observer.observe(document.querySelectorAll("#recipe-item"));*/
