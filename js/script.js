2cf762ef9f0996f1cf08eab5228f5339c7f50a21
Aras Pourbijan <arasmidos@gmail.com>
2022-09-23 14:24:13 +0200

    scroll done

--- js/script.js
+++ js/script.js
@@ -6,26 +6,149 @@ menuLista.classList.toggle("active")
 })
 
 
-// const contact=document.querySelectorAll('#contanct');
+// scroll animazione  ARAS ---------------------
 
-// window.addEventListener('scroll', checkEvent);
-
-// checkEvent();
-
-// function checkEvent(){
-//     const triggerbottom=console.log(window.innerHeight/5*4);
-
-// boxes.forEach(section => {
-//     const sectionTop= section.getBoundingClientRect().top;
-//     if(sectionTop <triggerbottom ){
-//       section.classList.add('show')   ;
-//     } else{
-//         section.classList..remove('show');
-//     }
+const observer = new IntersectionObserver(entries => {
+  
+  entries.forEach(entry => {
     
-// });
+    if (entry.isIntersecting) {
      
-// }
+      entry.target.classList.add('anime-footer--up');
+    }
+  });
+});
+observer.observe(document.querySelector('#footer-1-anim'));
+
+// ----------------------------------------
+
+const observer1 = new IntersectionObserver(entries => {
+  
+  entries.forEach(entry => {
+    
+    if (entry.isIntersecting) {
+     
+      entry.target.classList.add('anime-footer--up');
+    }
+  });
+});
+observer1.observe(document.querySelector('#anime-hr'));
+
+// ----------------------------------------
+const observer2 = new IntersectionObserver(entries => {
+  
+  entries.forEach(entry => {
+    
+    if (entry.isIntersecting) {
+     
+      entry.target.classList.add('anime-footer--right');
+    }
+  });
+});
+observer2.observe(document.querySelector('#footer-2-anim'));
+
+// ----------------------------------------
+const observer3 = new IntersectionObserver(entries => {
+  
+  entries.forEach(entry => {
+    
+    if (entry.isIntersecting) {
+     
+      entry.target.classList.add('anime-footer--down');
+    }
+  });
+});
+observer3.observe(document.querySelector('#footer-3-anim'));
+
+// ----------------------------------------
+const observer4 = new IntersectionObserver(entries => {
+  
+  entries.forEach(entry => {
+    
+    if (entry.isIntersecting) {
+     
+      entry.target.classList.add('anime-footer--down','delay06');
+    }
+  });
+});
+observer4.observe(document.querySelector('#footer-4-anim'));
+
+// ----------------------------------------
+const observer5 = new IntersectionObserver(entries => {
+  
+  entries.forEach(entry => {
+    
+    if (entry.isIntersecting) {
+     
+      entry.target.classList.add('anime-footer--down','delay09');
+    }
+  });
+});
+observer5.observe(document.querySelector('#footer-5-anim'));
+// ----------------------------------------
+const observer6 = new IntersectionObserver(entries => {
+  
+  entries.forEach(entry => {
+    
+    if (entry.isIntersecting) {
+     
+      entry.target.classList.add('form-btn');
+    }
+  });
+});
+observer6.observe(document.querySelector('#form-anime'));
+// ----------------------------------------
+const observer7 = new IntersectionObserver(entries => {
+  
+  entries.forEach(entry => {
+    
+    if (entry.isIntersecting) {
+     
+      entry.target.classList.add('anime-footer--right');
+    }
+  });
+});
+observer7.observe(document.querySelector('#box-contanct'));
+// ----------------------------------------
+const observer8 = new IntersectionObserver(entries => {
+  
+  entries.forEach(entry => {
+    
+    if (entry.isIntersecting) {
+     
+      entry.target.classList.add('anime-footer--right','delay08');
+    }
+  });
+});
+observer8.observe(document.querySelector('#latest-news'));
+// ----------------------------------------
+const observer9 = new IntersectionObserver(entries => {
+  
+  entries.forEach(entry => {
+    
+    if (entry.isIntersecting) {
+     
+      entry.target.classList.add('anime-footer--right','delay13');
+    }
+  });
+});
+observer9.observe(document.querySelector('#photo-and-social'));
+
+
+
+
+
+
+
+
+
+
+
+// fine scroll animazione  ARAS ---------------------
+
+
+// --------------------------------------
+
   menuLista.classList.toggle("active");
 
 
@@ -93,17 +216,17 @@ observer.observe(document.querySelector("#recipe-item"));*/
 
 // ---------------------------------------------------------------------------------
 
-const elementoDaAnimare = document.getElementById("carosello-bootstrap");
- const animazione = elementoDaAnimare.offsetTop;
+// const elementoDaAnimare = document.getElementById("carosello-bootstrap");
+//  const animazione = elementoDaAnimare.offsetTop;
 
-function AnimazioneScroll() {
-  if (window.pageYOffset >= animazione) {
-    elementoDaAnimare.classList.add("animation")
-  } else {
-    elementoDaAnimare.classList.remove("animation");
-  }
-}
-console.log(AnimazioneScroll())
+// function AnimazioneScroll() {
+//   if (window.pageYOffset >= animazione) {
+//     elementoDaAnimare.classList.add("animation")
+//   } else {
+//     elementoDaAnimare.classList.remove("animation");
+//   }
+// }
+// console.log(AnimazioneScroll())
 
 
 
