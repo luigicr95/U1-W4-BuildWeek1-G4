d6d44e827e0661695eddd852bc0b21d93dbbfb08
luigicr95 <112467906+luigicr95@users.noreply.github.com>
2022-09-23 13:03:04 +0200

    js tries

--- js/script.js
+++ js/script.js
@@ -2,9 +2,8 @@ const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const menuLista = document.getElementsByClassName("menu-lista")[0];
 
 toggleButton.addEventListener("click", () => {
-menuLista.classList.toggle("active")
-})
-
+  menuLista.classList.toggle("active");
+});
 
 // const contact=document.querySelectorAll('#contanct');
 
@@ -22,12 +21,11 @@ menuLista.classList.toggle("active")
 //     } else{
 //         section.classList..remove('show');
 //     }
-    
-// });
-     
-// }
-  menuLista.classList.toggle("active");
 
+// });
+
+// }
+menuLista.classList.toggle("active");
 
 // Create the observer
 
@@ -50,12 +48,11 @@ observer.observe(document.querySelector("#recipe-item"));*/
 //     if (contentPosition < screenPosition) {
 //       content.getElementsByClassName("")[0];
 //       console.log()
-//     }  
+//     }
 //     else {
 //         content.classList.remove("");
 //     }
 //     });
-
 
 // window.addEventListener("scroll",() => {
 //     let content = document.getElementById("about-us");
@@ -64,13 +61,12 @@ observer.observe(document.querySelector("#recipe-item"));*/
 //     console.log()
 //     if (contentPosition < screenPosition) {
 //         content.getElementsByClassName("animation")[0];
-//     }  
+//     }
 //     else {
 //         content.classList.remove("animation");
 //     }
 //     });
 // console.log()
-
 
 // -----------------------------------------------------
 
@@ -94,16 +90,62 @@ observer.observe(document.querySelector("#recipe-item"));*/
 // ---------------------------------------------------------------------------------
 
 const elementoDaAnimare = document.getElementById("carosello-bootstrap");
- const animazione = elementoDaAnimare.offsetTop;
-
-function AnimazioneScroll() {
-  if (window.pageYOffset >= animazione) {
-    elementoDaAnimare.classList.add("animation")
+const animazione = elementoDaAnimare.offsetTop;
+function animazioneScroll() {
+  if (window.pageYOffset > animazione) {
+    elementoDaAnimare.classList.add("animation");
   } else {
     elementoDaAnimare.classList.remove("animation");
   }
 }
-console.log(AnimazioneScroll())
 
+window.onscroll = function () {
+  animationOnScroll();
+};
+function animationOnScroll() {
+  let fadeInAnimation = document.getElementById("dish-one");
+  let animationApplied = fadeInAnimation.offsetTop;
+  if (window.pageYOffset >= animationApplied) {
+    fadeInAnimation.classList.add("dish-1");
+  }
+}
 
+function animationOnScrollOne() {
+  let fadeInAnimation = document.getElementById("dish-two");
+  let animationApplied = fadeInAnimation.offsetTop;
+  if (window.pageYOffset >= animationApplied) {
+    fadeInAnimation.classList.add("dish-2");
+  }
+}
 
+function animationOnScrollTwo() {
+  const fadeInAnimation = document.getElementById("dish-three");
+  const animationApplied = fadeInAnimation.offsetTop;
+  if (window.pageYOffset >= animationApplied) {
+    fadeInAnimation.classList.add("dish-3");
+  }
+}
+
+function animationOnScrollThree() {
+  let fadeInAnimation = document.getElementById("dish-four");
+  let animationApplied = fadeInAnimation.offsetTop;
+  if (window.pageYOffset >= animationApplied) {
+    fadeInAnimation.classList.add("dish-4");
+  }
+}
+
+function animationOnScrollFour() {
+  let fadeInAnimation = document.getElementById("dish-five");
+  let animationApplied = fadeInAnimation.offsetTop;
+  if (window.pageYOffset >= animationApplied) {
+    fadeInAnimation.classList.add("dish-5");
+  }
+}
+
+function animationOnScrollFive() {
+  let fadeInAnimation = document.getElementById("dish-six");
+  let animationApplied = fadeInAnimation.offsetTop;
+  if (window.pageYOffset >= animationApplied) {
+    fadeInAnimation.classList.add("dish-6");
+  }
+}
